c6b9111239eec5be6fffe6abe9458848a7976126
Park Byung Eun <geekdev@naver.com>
2024-11-03 22:01:35 +0900

    06스택-문제 11번 조건문 수정

--- src/main/stack/06-11.js
+++ src/main/stack/06-11.js
@@ -21,15 +21,13 @@
 export default function solution(s) {
   const stack = [];
 
-  for (let i = 0; i < s.length; i++) {
-    if (stack.length === 0) {
-      stack.push(s[i]);
+  stack.push(s[0]);
+
+  for (let i = 1; i < s.length; i++) {
+    if (stack[stack.length - 1] === s[i]) {
+      stack.pop();
     } else {
-      if (stack[stack.length - 1] === s[i]) {
-        stack.pop();
-      } else {
-        stack.push(s[i]);
-      }
+      stack.push(s[i]);
     }
   }
 
